57b096c86ba20e10c3804e3462eca79fed1cf23e
parth2021singh <parth_singh@persistent.com>
2022-11-25 15:47:09 +0530

    first commit

---
+++ src/components/Header/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div style={{width:"400px",height:"50px",border:"2px solid red",display:"flex",justifyContent:"center",alignItems:"center"}}>
+        <span>Header for test</span>
+    </div>
+  )
+}
+
+export default Header
