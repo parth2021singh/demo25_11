46081f03ccf77c2355f507cd0fe604b6d40a4812
parth2021singh <parth_singh@persistent.com>
2022-11-25 16:59:20 +0530

    background-color added

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div style={{width:"400px",height:"50px",border:"2px solid red",display:"flex",justifyContent:"center",alignItems:"center"}}>
+    <div style={{width:"400px",height:"50px",backgroundColor:"yellow",border:"2px solid red",display:"flex",justifyContent:"center",alignItems:"center"}}>
         <span>Header for test</span>
     </div>
   )
