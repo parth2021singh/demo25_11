c8d1efba63383b735000b5dbbe5d343a16ed95c0
parth2021singh <parth_singh@persistent.com>
2022-11-28 10:42:05 +0530

    m1

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <div  style={{display:"flex",flexDirection:"column",justifyContent:"center",alignItems:"center",padding:"10px 10px"}}>
-    <div style={{width:"1100px",height:"100px",backgroundColor:"red",border:"4px solid red",color:"white",display:"flex",justifyContent:"center",alignItems:"center"}}>
+    <div style={{width:"1100px",height:"100px",backgroundColor:"red",border:"2px solid red",color:"white",display:"flex",justifyContent:"center",alignItems:"center"}}>
     Velit exercitation exercitation ipsum deserunt id ullamco. Dolore minim voluptate proident deserunt dolor nisi non nulla et dolore adipisicing sint do. Amet nulla id exercitation nisi occaecat tempor. Exercitation labore aliqua pariatur Lorem aute velit occaecat ea adipisicing incididunt ipsum aliquip amet.
     </div>
     </div>
